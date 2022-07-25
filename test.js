b0b12664491853792c5f47cf9c0c6b38de874f1e
sharique001 <sharique.immanent@gmail.com>
2022-07-25 14:38:13 +0530

    update

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const parwinder = {
     name: "parwinder"
 }
 UserDB.addUser(parwinder)
-//console.log(UserDB.users)
+console.log(UserDB.users)
 
 // UserDB.findUser(parwinder)
 // console.log(UserDB.users)
